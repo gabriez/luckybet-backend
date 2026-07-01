02c04bb7bd91fcd859405c7f15f6aad2c56778e5
gabotdev <gabrieltorres2014@gmail.com>
2026-07-01 01:12:07 -0400

    adding biome

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [ConfigModule.forRoot({ cache: true, isGlobal: true }), UsersModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	app.use(cookieParser());
+	await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch(handleError);
+
+function handleError(error: unknown) {
+	// eslint-disable-next-line unicorn/no-process-exit
+	process.exit(1);
+}
+
+process.on('uncaughtException', handleError);
